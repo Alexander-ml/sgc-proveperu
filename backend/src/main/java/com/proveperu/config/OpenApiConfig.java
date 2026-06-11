c6d56a37ea10baa94dd1a1a7b2143a9f1465ad4f
David <sanchezordonezdavid7@gmail.com>
2026-06-10 22:16:04 -0500

    corregir algunos errores (no muchos)

--- backend/src/main/java/com/proveperu/config/OpenApiConfig.java
+++ backend/src/main/java/com/proveperu/config/OpenApiConfig.java
@@ -1,12 +1,19 @@
 package com.proveperu.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
