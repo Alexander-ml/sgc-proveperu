3722a3c743fa40991758faf968d362f7d4207243
David <sanchezordonezdavid7@gmail.com>
2026-06-12 13:15:38 -0500

    feat(usuarios): actualizar configuracion de OpenAPI y modulo de permisos

--- backend/src/main/java/com/proveperu/config/OpenApiConfig.java
+++ backend/src/main/java/com/proveperu/config/OpenApiConfig.java
@@ -5,15 +5,10 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
-@OpenAPIDefinition(
-        security = {
-                @SecurityRequirement(name = "bearerAuth")
-        }
-)
+@OpenAPIDefinition
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
